52f4a73f15040a0c7f904647dd5cad7aae0a35f9
Faesalkhan <pathanf9545@gmail.com>
2024-02-01 12:07:44 +0530

    logical programs

---
+++ logicalPrograms.js
@@ -0,0 +1,28 @@
+//swapping of two integers -------------------------------------------------------
+// let a = 5;
+// let b = 10;
+// console.log(`before swapping a : ${a} and b : ${b}`);
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(`after swapping a : ${a} and b : ${b}`);
+
+// swapping of two integers without 3rd variable -------------------------------------------------------
+// let a = 10;
+// let b = 20;
+// console.log(`before swapping, a : ${a} and b : ${b}`);
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(`after swapping, a: ${a} and b: ${b}`);
+
+// check if no. is even/not without using % operator-------------------------------------------------------
+// function isEven(n) {
+//   return (n & 1) === 0;
+// }
+// let num = 4;
+// if (isEven(num)) {
+//   console.log(`${num} is even`);
+// } else {
+//   console.log(`${num} is odd`);
+// }
