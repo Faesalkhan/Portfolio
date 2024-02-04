30369818e2789da886f0055df9802999c758e548
Faesalkhan <pathanf9545@gmail.com>
2024-02-04 18:01:36 +0530

    logical programs

--- logicalPrograms.js
+++ logicalPrograms.js
@@ -26,3 +26,35 @@
 // } else {
 //   console.log(`${num} is odd`);
 // }
+
+// allow the user to enter a input integer -------------------------------------------------------
+// let userInput;
+// do {
+//   userInput = prompt("enter a no.:");
+//   userInput = parseInt(userInput);
+// } while (isNaN(userInput) || !Number.isInteger(userInput));
+// console.log(`you entered: ${userInput}`);
+
+//or
+
+// let input = parseInt(prompt("enter a number"));
+// if (!isNaN(input) && input > 0) {
+//   console.log(input);
+// } else {
+//   console.log("plz enter a + number");
+// }
+
+// adding two numbers -------------------------------------------------------
+// let num1 = parseFloat(prompt("enter the first number :"));
+// let num2 = parseFloat(prompt("enter the second number :"));
+// if (!isNaN(num1) && !isNaN(num2)) {
+//   let sum = num1 + num2;
+//   console.log("addition is : " + sum);
+// } else {
+//   console.log("plz enter the valid numbers");
+// }
+
+// to calculate the string length-------------------------------------------------------
+// let inputString = prompt("enter a string");
+// let stringLength = inputString.length;
+// console.log(stringLength);
